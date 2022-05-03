416be82bc44703cf697fb089732c76ef5692783a
klim2020 <klimprograms@gmail.com>
2022-05-03 07:13:22 +0300

    ---

--- resources/render/apivue/scripts/gh-pages-deploy.js
+++ resources/render/apivue/scripts/gh-pages-deploy.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
-import execa from "execa";
-import fs from "fs";
+//import {execa} from "execa";
+const execa = require('execa');
+const fs = require('fs');
 
 (async () => {
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
-        //await execa("npm", ["run", "build"]);
+        await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
