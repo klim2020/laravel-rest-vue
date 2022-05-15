23716c32957720e8a8e84ce173684cdd0dd53263
klim2020 <klimprograms@gmail.com>
2022-05-15 09:29:11 +0300

    +added languagecomponents

--- resources/render/apivue/scripts/gh-pages-deploy.mjs
+++ resources/render/apivue/scripts/gh-pages-deploy.mjs
@@ -10,15 +10,18 @@ import fs from 'fs';
         // eslint-disable-next-line no-console
         console.log("Building started...");
         await execa("npm", ["run", "build"]);
+
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        await execa("cp",  ["src/assets/cname/CNAME",folderName]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folderName]);
         await execa("git", ["checkout", "-f", "master"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
+
         console.log("Successfully deployed, check your settings");
     } catch (e) {
         // eslint-disable-next-line no-console
