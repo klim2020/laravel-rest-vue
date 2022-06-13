8f2c75b5d1dd754c088b2de27f00ab867c29f2e9
klim2020 <klimprograms@gmail.com>
2022-06-13 11:52:13 +0300

    +commented unused menu items

--- resources/render/apivue/src/components/VueHeader.vue
+++ resources/render/apivue/src/components/VueHeader.vue
@@ -27,23 +27,23 @@
                     <li class="hidden sm:flex">
                         <router-link class="text-white hover:text-gray-400 text font-semibold py-1 px-3" to="/cv">Portfolio</router-link>
                     </li>
-                    <li class="hidden sm:flex">
+         <!--       <li class="hidden sm:flex">
                         <router-link class="text-white hover:text-gray-400 text font-semibold py-1 px-3" to="/about">About</router-link>
-                    </li>
-                    <li class="hidden sm:flex">
+                    </li> -->
+          <!--      <li class="hidden sm:flex">
                         <div class="dropdown inline-block relative">
-                            <button class="text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                                <span class="mr-1">Articles</span>
-                                <svg class="fill-current h-4 w-4 transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
-                            </button>
-                            <ul class="bg-emerald-400 dropdown-menu absolute hidden text-white pt-1
-                                       ">
-                                <li class=""><a class="rounded-t bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">One</a></li>
-                                <li class=""><a class="bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">Two</a></li>
-                                <li class=""><a class="rounded-b bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">Three is the magic number</a></li>
-                            </ul>
+                             <button class="text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+                                  <span class="mr-1">Articles</span>
+                                  <svg class="fill-current h-4 w-4 transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                              </button>
+                              <ul class="bg-emerald-400 dropdown-menu absolute hidden text-white pt-1
+                                                 ">
+                                  <li class=""><a class="rounded-t bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">One</a></li>
+                                  <li class=""><a class="bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">Two</a></li>
+                                  <li class=""><a class="rounded-b bg-emerald-400 hover:text-gray-400 text font-semibold py-2 px-4 block whitespace-no-wrap" href="#">Three is the magic number</a></li>
+                              </ul>
                         </div>
-                    </li>
+                    </li>-->
                 </ul>
                 <ul class="flex grow md:grow-0 space-x-4">
                     <LanguageSelector  @languageChange="onSwitchlanguage"></LanguageSelector>
--- resources/render/apivue/src/router/index.js
+++ resources/render/apivue/src/router/index.js
@@ -9,19 +9,20 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about/:lang*',
-    name: 'About',
+  //{
+//    path: '/about/:lang*',
+//    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path:'/data/:lang*',
-    name:'SimpleData',
-    component: SimpleData
-  },
+//    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//  },
+
+ // {
+ //   path:'/data/:lang*',
+ //   name:'SimpleData',
+ //   component: SimpleData
+ // },
     {
         path:'/cv/:lang*',
         name:'CV',
